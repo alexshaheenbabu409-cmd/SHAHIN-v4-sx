74e3eab1ca995d80182acc5fd4fce3e68890cb00
alexshaheenbabu409-cmd <alexshaheenbabu409@gmail.com>
2026-06-30 11:22:42 +0600

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import NobitaChat from './NobitaChat.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <NobitaChat />
-  </React.StrictMode>
-);
