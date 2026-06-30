a3c631f5db908b69c5f191b10ddec26d1725a9a7
alexshaheenbabu409-cmd <alexshaheenbabu409@gmail.com>
2026-06-30 11:23:28 +0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NobitaChat from './NobitaChat.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NobitaChat />
+  </React.StrictMode>
+);
